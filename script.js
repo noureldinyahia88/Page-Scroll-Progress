0b13486648d4101b7e3584b7e0ed2c2860cd6695
nour <noureldinyahia@gmail.cpm>
2023-03-07 16:59:15 +0200

    done

---
+++ script.js
@@ -0,0 +1,7 @@
+let el = document.querySelector('.scroll')
+let height = document.documentElement.scrollHeight - document.scrollingElement.clientHeight;
+
+window.addEventListener('scroll',()=>{
+    let scrollTop = document.documentElement.scrollTop;
+    el.style.width = `${(scrollTop / height) * 100}%`;
+})
